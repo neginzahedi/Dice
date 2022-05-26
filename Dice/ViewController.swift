1ae55ddac08124dbb7807a7f787bc486c4ec3e90
Negin Zahedi <zahedi.negin@gmail.com>
2022-05-26 14:15:05 -0400

    add dices and button

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var DiceOne: UIImageView!
+    @IBOutlet weak var DiceTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func throwDice(_ sender: Any) {
+ 
+    }
+    
 }
 
